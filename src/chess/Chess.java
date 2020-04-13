e0ea1f736763bbb2d97f2688ae4b82320042a82f
Dimo <dimo_marino@abv.bg>
2020-04-13 18:54:04 +0300

    finalize_texture_manager

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,14 +1,27 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Chess {
 
     private Display display;
+    Scanner input = new Scanner(System.in);
+    TextureManager t;
 
     public void init() {
         display = new Display(600, 600, "Chess");
+        t = new TextureManager(30,30,50,50,
+                "D:\\CourseJavaProjects\\Chess\\src\\assets\\sprite2.png");
+        display.addGraphic(t);
     }
 
     public void update() {
+
+        t.render();
+
+        t.setX(input.nextInt());
+        t.setY(input.nextInt());
+
     }
 
     public void render() {
