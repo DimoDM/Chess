0ad6e8cef66c6c2277530b1ad3882bf36db9775c
Dimo <dimo_marino@abv.bg>
2020-04-17 15:44:32 +0300

    forget_branches-add_input_manager_in_this_way

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -6,10 +6,12 @@ public class Chess {
 
     public static Display display;
     Board board;
+    InputManager input;
 
     public void init() {
         display = new Display(600, 600, "Chess");
         board = new Board();
+        input = new InputManager();
     }
 
     public void update() {
