4bf4feff080749d3803fc348f5abb9ecfad0ee12
Dimo <dimo_marino@abv.bg>
2020-04-21 23:09:56 +0300

    finalize_king_movement

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -11,6 +11,7 @@ public class Board {
     public PlayingPiece[][] board = new PlayingPiece[8][8];
     public Vector2D whiteKingCords = new Vector2D();
     public Vector2D blackKingCords = new Vector2D();
+    private boolean whiteMove = true;
 
     Board() {
         init();
@@ -22,7 +23,7 @@ public class Board {
             board[1][i] = new Pawn(PlayingPiece.WIDTH * i, PlayingPiece.HEIGHT, "src\\assets\\blackPawn.png");
             board[6][i] = new Pawn(PlayingPiece.WIDTH * i, PlayingPiece.HEIGHT * 6, "src\\assets\\whitePawn.png");
         }
-        board[0][0] = new Rook(0,0,"src\\assets\\blackRook.png");
+        board[0][0] = new Rook(0, 0, "src\\assets\\blackRook.png");
         board[0][1] = new Knight(figureWidth, 0, "src\\assets\\blackKnight.png");
         board[0][2] = new Bishop(figureWidth * 2, 0, "src\\assets\\blackBishop.png");
         board[0][3] = new Queen(figureWidth * 3, 0, "src\\assets\\blackQueen.png");
@@ -31,9 +32,9 @@ public class Board {
         blackKingCords.y = 0;
         board[0][5] = new Bishop(figureWidth * 5, 0, "src\\assets\\blackBishop.png");
         board[0][6] = new Knight(figureWidth * 6, 0, "src\\assets\\blackKnight.png");
-        board[0][7] = new Rook(figureWidth * 7,0,"src\\assets\\blackRook.png");
+        board[0][7] = new Rook(figureWidth * 7, 0, "src\\assets\\blackRook.png");
 
-        board[7][0] = new Rook(0,figureWidth * 7,"src\\assets\\whiteRook.png");
+        board[7][0] = new Rook(0, figureWidth * 7, "src\\assets\\whiteRook.png");
         board[7][1] = new Knight(figureWidth, figureWidth * 7, "src\\assets\\whiteKnight.png");
         board[7][2] = new Bishop(figureWidth * 2, figureWidth * 7, "src\\assets\\whiteBishop.png");
         board[7][3] = new Queen(figureWidth * 3, figureWidth * 7, "src\\assets\\whiteQueen.png");
@@ -42,15 +43,15 @@ public class Board {
         whiteKingCords.y = 7;
         board[7][5] = new Bishop(figureWidth * 5, figureWidth * 7, "src\\assets\\whiteBishop.png");
         board[7][6] = new Knight(figureWidth * 6, figureWidth * 7, "src\\assets\\whiteKnight.png");
-        board[7][7] = new Rook(figureWidth * 7,figureWidth * 7,"src\\assets\\whiteRook.png");
+        board[7][7] = new Rook(figureWidth * 7, figureWidth * 7, "src\\assets\\whiteRook.png");
         for (int i = 2; i < board.length - 2; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = new EmptyFigure(i * figureWidth, j*figureWidth, "src\\assets\\empty.png");
+                board[i][j] = new EmptyFigure(i * figureWidth, j * figureWidth, "src\\assets\\empty.png");
             }
         }
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                Chess.display.addGraphic(board[i][j].getTexture());
+        for (PlayingPiece[] playingPieces : board) {
+            for (PlayingPiece playingPiece : playingPieces) {
+                Chess.display.addGraphic(playingPiece.getTexture());
             }
         }
         Chess.display.addGraphic(boardTex);
@@ -69,13 +70,27 @@ public class Board {
     }
 
     public void move(int currX, int currY, int nextX, int nextY) {
-        if(board[currY][currX].isLegalMove(currX, currY, nextY, nextX)) {
-
-            Chess.display.removeGraphic(board[nextY][nextX].getTexture());
-            board[nextY][nextX] = board[currY][currX];
-            board[currY][currX] = new EmptyFigure(currX * PlayingPiece.WIDTH,currY * PlayingPiece.HEIGHT,"src\\assets\\empty.png");
-        } else System.out.println("can't move");
+        if (board[currY][currX].getColor() == (whiteMove ? 'w' : 'b')) {
+            if (board[currY][currX].isLegalMove(currX, currY, nextY, nextX)) {
+                moveFigureInBoardLogic(currX, currY, nextX, nextY);
+                if (isKingUnderThreat(getKing().getX() / PlayingPiece.HEIGHT, getKing().getY() / PlayingPiece.WIDTH, getKing())) {
+                    moveFigureInBoardLogic(nextX, nextY, currX, currY);
+                    System.out.println("the king is under check");
+                }
+                else whiteMove = !whiteMove;
+            } else System.out.println("can't move");
+        }
         update();
+    }
+
+    private King getKing() {
+        return whiteMove ? (King) board[whiteKingCords.y][whiteKingCords.x] : (King) board[blackKingCords.y][blackKingCords.x];
+    }
+
+    private void moveFigureInBoardLogic(int currX, int currY, int nextX, int nextY) {
+        Chess.display.removeGraphic(board[nextY][nextX].getTexture());
+        board[nextY][nextX] = board[currY][currX];
+        board[currY][currX] = new EmptyFigure(currX * PlayingPiece.WIDTH, currY * PlayingPiece.HEIGHT, "src\\assets\\empty.png");
     }
 
     public boolean isKingUnderThreat(int kingX, int kingY, King king) {
@@ -88,17 +103,17 @@ public class Board {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 PlayingPiece figure = board[i][j];
-                if(figure.getColor() != king.getColor() && figure.getColor() != ' ')
-                //System.out.println("test: " + figure.path + " i: " + i + " j: " + j + " board-length: " + board.length + " board[i]-length: " + board[i].length );
-                if(figure.isLegalMove(figure.getX() / PlayingPiece.WIDTH, figure.getY() / PlayingPiece.HEIGHT, kingY, kingX)) {
-                    board[kingY][kingX] = help;
-                    board[king.getY() / PlayingPiece.HEIGHT][king.getX() / PlayingPiece.WIDTH] = help2;
-                    System.out.println("threat: " + figure.path);
-                    return true;
-                }
+                if (figure.getColor() != king.getColor() && figure.getColor() != ' ')
+                    //System.out.println("test: " + figure.path + " i: " + i + " j: " + j + " board-length: " + board.length + " board[i]-length: " + board[i].length );
+                    if (figure.isLegalMove(figure.getX() / PlayingPiece.WIDTH, figure.getY() / PlayingPiece.HEIGHT, kingY, kingX)) {
+                        board[kingY][kingX] = help;
+                        board[king.getY() / PlayingPiece.HEIGHT][king.getX() / PlayingPiece.WIDTH] = help2;
+                        System.out.println("threat: " + figure.path);
+                        return true;
+                    }
             }
         }
-        System.out.println("help: " + help.path);
+        //System.out.println("help: " + help.path);
         board[kingY][kingX] = help;
         board[king.getY() / PlayingPiece.HEIGHT][king.getX() / PlayingPiece.WIDTH] = help2;
 
